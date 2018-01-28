1494dd78bbe49f6ebb888793f89a3362e8718fa6
Jan Marucha <jan.marucha@magd.ox.ac.uk>
2018-01-28 22:59:59 +0000

    too much stuff done to remember

--- react_dict.py
+++ react_dict.py
@@ -6,4 +6,7 @@ class ReactDict(dict):
             self[reciever_id][giver_id] = {}
         if react_type not in self[reciever_id][giver_id]:
             self[reciever_id][giver_id][react_type] = 0
+        if "total" not in self[reciever_id][giver_id]:
+            self[reciever_id][giver_id]["total"] = 0
         self[reciever_id][giver_id][react_type] += 1
+        self[reciever_id][giver_id]["total"] += 1
